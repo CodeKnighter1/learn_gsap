23b114445a118680e8019213b5bc9d7c34c7656c
CodeKnighter1 <rahmonqulqoraboyev01@gmail.com>
2026-03-09 09:17:07 +0500

    implement the menu section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -66,7 +66,7 @@ function Hero() {
     return (
         <>
             <section id="hero" className="noisy">
-                <h1 className="title">Gorilla</h1>
+                <h1 className="title">Moxito</h1>
 
                 <img src="/images/hero-left-leaf.png" alt="left leaf" className="left-leaf" />
                 <img src="/images/hero-right-leaf.png" alt="right leaf" className="right-leaf" />
